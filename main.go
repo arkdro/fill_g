3c67572be325677336a39bf9645b280f52278c17
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-09 23:40:04 +0100

    preparing iterate over files

--- main.go
+++ main.go
@@ -3,12 +3,47 @@ package main
 import (
 	"github.com/asdf/fill_g/fill"
 	"flag"
+	"log"
+	"io/ioutil"
 )
 
 func main() {
 	var file = flag.String("infile", "", "input file")
 	var dir = flag.String("indir", "", "input file")
 	flag.Parse()
-	fill.Run(*file, *dir)
+	run(*file, *dir)
+}
+
+func run(file string, dir string) {
+	files := make([]string, 0)
+	if file != "" {
+		files = append(files, file)
+	} else if dir != "" {
+		files = get_files_in_dir(dir)
+	} else {
+		log.Printf("No parameters given")
+		return
+	}
+	process_files(files)
+}
+
+func get_files_in_dir(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Printf("get_files_in_dir, read dir error: %v", err)
+		files := make([]string, 0)
+		return files
+	}
+	names := make([]string, 0)
+	for _, file := range files {
+		names = append(names, file.Name())
+	}
+	return names
+}
+
+func process_files(files []string) {
+	for _, file := range files {
+		log.Printf("process_files, file: %v", file)
+	}
 }
 
