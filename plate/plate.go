7b75f7b6d1d44b4ffeb5d6ada3c1c01cf691bfbd
Arkadiy Drovosekov <arkdro@gmail.com>
2017-11-11 22:58:43 +0100

    get/set color for plate

--- plate/plate.go
+++ plate/plate.go
@@ -1,8 +1,25 @@
 package plate
 
+import (
+	"github.com/asdf/fill_g/node"
+)
+
 type Plate struct {
 	Width int
 	Height int
 	Data [][]int
+}
+
+func (plate Plate) Get_color(node node.Node) int {
+	x := node.X
+	y := node.Y
+	color := plate.Data[y][x]
+	return color
+}
+
+func (plate *Plate) Set_color(node node.Node, color int) {
+	x := node.X
+	y := node.Y
+	plate.Data[y][x] = color
 }
 
